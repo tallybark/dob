cea31d24bc103f785fd4b7e5318fa41967e6ee38
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-06-12 22:56:19 -0500

    Rebrand: Legal: Update copyright headers: 'hamster_cli' → 'dob'.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# This file is part of 'hamster_cli'.
+# This file is part of 'dob'.
 #
-# 'hamster_cli' is free software: you can redistribute it and/or modify
+# 'dob' is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# 'hamster_cli' is distributed in the hope that it will be useful,
+# 'dob' is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with 'hamster_cli'.  If not, see <http://www.gnu.org/licenses/>.
+# along with 'dob'.  If not, see <http://www.gnu.org/licenses/>.
 
 """Testsuite for ``hamster_cli``."""
 
