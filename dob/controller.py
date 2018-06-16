a2d16ac721f40b2ff287126e94488ab943f68e3f
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-06-16 14:20:55 -0500

    Refactor: Descope functions' sub-functions.
    - I.e., make private functions even more private;
      and group logically related code "physically"
      code together.

--- dob/controller.py
+++ dob/controller.py
@@ -77,55 +77,41 @@ class Controller(HamsterControl):
         else:
             return bool(self.store.get_db_url())
 
-    def _adjust_log_level(self, nark_config):
-        # *cough*hack!*cough*”
-        # Because invoke_without_command, we allow command-less invocations.
-        #   For one such invocation -- dob -v -- tell the store not to log.
-        # Also tell the store not to log if user did not specify anything,
-        #   because we'll show the help/usage (which Click would normally
-        #   handle if we had not tampered with invoke_without_command).
-        if (
-            (len(sys.argv) > 2) or
-            ((len(sys.argv) == 2) and (sys.argv[1] not in ('-v', 'version')))
-        ):
-            return
-        nark_config['sql_log_level'] = 'WARNING'
-
     def insist_germinated(self):
         """Assist user if config or database not present."""
-        store_exists = self.store_exists
-        if self.cfgfile_exists and store_exists:
-            self.standup_store()
-            return
-        if not self.cfgfile_exists and not store_exists:
-            self.welcome_newbie()
-        else:
-            self.berate_user_files_unwell(store_exists)
-        echo_copyright()
-        sys.exit(1)
+        def _insist_germinated():
+            store_exists = self.store_exists
+            if self.cfgfile_exists and store_exists:
+                self.standup_store()
+                return
+            if not self.cfgfile_exists and not store_exists:
+                help_newbie_onboard()
+            else:
+                berate_user_files_unwell(store_exists)
+            echo_copyright()
+            sys.exit(1)
 
-    def welcome_newbie(self):
-        self.help_newbie_onboard()
+        def help_newbie_onboard():
+            click.echo(
+                help_strings.NEWBIE_HELP_ONBOARDING.format(
+                    legacy_help=upgrade_legacy_database_instructions(self),
+                ),
+                err=True,
+            )
 
-    def berate_user_files_unwell(self, store_exists):
-        if not self.cfgfile_exists:
-            self.oblige_user_create_config()
-        if not store_exists:
-            self.oblige_user_create_store()
+        def berate_user_files_unwell(store_exists):
+            if not self.cfgfile_exists:
+                oblige_user_create_config()
+            if not store_exists:
+                oblige_user_create_store()
 
-    def help_newbie_onboard(self):
-        click.echo(
-            help_strings.NEWBIE_HELP_ONBOARDING.format(
-                legacy_help=upgrade_legacy_database_instructions(self),
-            ),
-            err=True,
-        )
+        def oblige_user_create_config():
+            click.echo(help_strings.NEWBIE_HELP_CREATE_CONFIG, err=True)
 
-    def oblige_user_create_config(self):
-        click.echo(help_strings.NEWBIE_HELP_CREATE_CONFIG, err=True)
+        def oblige_user_create_store():
+            click.echo(help_strings.NEWBIE_HELP_CREATE_STORE, err=True)
 
-    def oblige_user_create_store(self):
-        click.echo(help_strings.NEWBIE_HELP_CREATE_STORE, err=True)
+        _insist_germinated()
 
     def create_config(self, force):
         exists = os.path.exists(get_config_path())
@@ -164,23 +150,12 @@ class Controller(HamsterControl):
             # like create_config_maybe, which tells the user if the things exists
             # already, because the storage class, SQLAlchemyStore, blindly calls
             # create_all (in create_storage_tables) without checking if db exists.
-            skip_standup = check_sqlite_store_ready()
+            skip_standup = self._check_sqlite_store_ready()
             if not skip_standup:
                 self.standup_store()
                 click.echo(
                     _('Dob database is ready at {}').format(self.store.get_db_url())
                 )
-
-        def check_sqlite_store_ready():
-            if self.config['db_engine'] != 'sqlite':
-                return False
-            db_path = self.config['db_path']
-            if not os.path.isfile(db_path):
-                return False
-            click.echo(
-                _('Data store already exists at {}').format(db_path)
-            )
-            return True
 
         def exit_already_germinated():
             dob_in_user_exit(_(
@@ -188,4 +163,29 @@ class Controller(HamsterControl):
             ).format(__arg0name__))
 
         _create_config_and_store()
+
+    def _adjust_log_level(self, nark_config):
+        # *cough*hack!*cough*”
+        # Because invoke_without_command, we allow command-less invocations.
+        #   For one such invocation -- dob -v -- tell the store not to log.
+        # Also tell the store not to log if user did not specify anything,
+        #   because we'll show the help/usage (which Click would normally
+        #   handle if we had not tampered with invoke_without_command).
+        if (
+            (len(sys.argv) > 2) or
+            ((len(sys.argv) == 2) and (sys.argv[1] not in ('-v', 'version')))
+        ):
+            return
+        nark_config['sql_log_level'] = 'WARNING'
+
+    def _check_sqlite_store_ready(self):
+        if self.config['db_engine'] != 'sqlite':
+            return False
+        db_path = self.config['db_path']
+        if not os.path.isfile(db_path):
+            return False
+        click.echo(
+            _('Data store already exists at {}').format(db_path)
+        )
+        return True
 
