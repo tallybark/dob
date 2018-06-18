cf09d1c0e0aa79052637a10b279e994b8c09baa3
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-06-17 22:50:31 -0500

    Pasted: Copy confirm() from PPT's shortcuts.

---
+++ dob/helpers/re_confirm.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of 'dob'.
+#
+# 'dob' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'dob' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'dob'.  If not, see <http://www.gnu.org/licenses/>.
+"""Facts Carousel"""
+
+from __future__ import absolute_import, unicode_literals
+
+from six import text_type
+
+from prompt_toolkit.formatted_text import merge_formatted_text
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.shortcuts import PromptSession
+
+__all__ = [
+    'confirm',
+    # Private:
+    #  'create_confirm_session',
+]
+
+# (lb): This is ripped from PPT's shortcuts/prompt.py
+
+
+create_confirm_session(message, suffix=' (y/n) '):
+    """
+    Create a `PromptSession` object for the 'confirm' function.
+    """
+    assert isinstance(message, text_type)
+    bindings = KeyBindings()
+
+    @bindings.add('y')
+    @bindings.add('Y')
+    def yes(event):
+        session.default_buffer.text = 'y'
+        event.app.exit(result=True)
+
+    @bindings.add('n')
+    @bindings.add('N')
+    @bindings.add('c-c')
+    def no(event):
+        session.default_buffer.text = 'n'
+        event.app.exit(result=False)
+
+    @bindings.add(Keys.Any)
+    def _(event):
+        " Disallow inserting other text. "
+        pass
+
+    complete_message = merge_formatted_text([message, suffix])
+    session = PromptSession(complete_message, key_bindings=bindings)
+    return session
+
+
+def confirm(message='Confirm?', suffix=' (y/n) '):
+    """
+    Display a confirmation prompt that returns True/False.
+    """
+    session = create_confirm_session(message, suffix)
+    return session.prompt()
+
