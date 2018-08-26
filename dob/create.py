3add91ae5f3d4fca04d896a0378e5f6bd89c5960
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-08-26 14:05:56 -0500

    Overhaul: create.py fcns: Use Carousel; Smart time mending; And More! (#carousel-overhaul)
    - In create.add_fact(), add option to use Carousel instead of
      Awesome Prompt, and default to using Carousel.
      - Also, mend new fact times per neighbor times (i.e., change
        +/- relative times, and add dates to clock times, etc.).
    - Refactor create.mend_facts_confirm_and_save_maybe().
      - Mend times per neighbors; and confirm conflicts.
    - New fcn., create.mend_fact_timey_wimey().
      - Fills in the start and end times of a new
        fact and checks for conflicts based on both
        the data store, and on other edited facts.
    - Uses new PlaceableFact, rather than Fact, which decoupled
      editing meta (like dirty_reasons, and source and line hints)
      from underlying, pure fact data (like activity and description).
    - Update create.save_fact().
      - Add ignore_pks params.
      - Ignore deleted Facts with no PK.

--- dob/create.py
+++ dob/create.py
@@ -20,26 +20,54 @@ from __future__ import absolute_import, unicode_literals
 from gettext import gettext as _
 
 import click
+import glob
 import json
+import os
+import pygments.lexers
+import traceback
 from inflector import Inflector, English
+from io import open
 
-from nark import Fact
+# FIXME: Move this to an intermediate carousel-managing class.
+#        (lb): That is, decouple PPT implementation from create.py?
+from prompt_toolkit.lexers import PygmentsLexer
+
 from nark.helpers.colored import fg, attr
 from nark.helpers.parsing import ParserException
 
 from . import interrogate
-from .helpers import click_echo, dob_in_user_exit
-from .helpers.fix_times import mend_facts_times, must_complete_times
+from .cmd_common import echo_block_header
+from .cmd_config import get_appdirs_subdir_file_path, AppDirs
+from .helpers import (
+    click_echo,
+    dob_in_user_exit,
+    dob_in_user_warning,
+    echo_fact,
+    highlight_value
+)
+from .helpers.crude_progress import CrudeProgress
+from .helpers.fix_times import (
+    mend_facts_times,
+    must_complete_times,
+    reduce_time_hint,
+    unite_and_stretch
+)
+from .helpers.path import compile_and_eval_source
+from .traverser import various_lexers
+from .traverser import various_styles
+from .traverser.placeable_fact import PlaceableFact
 
 __all__ = [
     'add_fact',
-    'stop_fact',
     'cancel_fact',
     'mend_facts_confirm_and_save_maybe',
-    'save_facts_maybe',
+    'stop_fact',
     # Private:
+    #   'echo_ongoing_completed',
+    #   'mend_fact_timey_wimey',
     #   'must_confirm_fact_edits',
     #   'must_create_fact_from_factoid',
+    #   'save_facts_maybe',
 ]
 
 
@@ -50,6 +78,7 @@ def add_fact(
     ask=False,
     yes=False,
     dry=False,
+    use_carousel=True,
 ):
     """
     Start or add a fact.
@@ -64,6 +93,11 @@ def add_fact(
             'verify_both': Expect to find both start and end times.
             'verify_start': Expect to find just one time, which is the start.
             'verify_end': Expect to find just one time, which is the end.
+            'verify_then': Optional time is new start; and either extend
+                            ongoing fact to new start, or back-fill interval gap.
+            'verify_still': Optional time is new start; copy prev meta to new Fact;
+                            either extend ongoing fact, or back-fill interval gap.
+            'verify_after': No time spec. Start new Fact at time of previous end.
 
         yes (bool, optional): If True, update other Facts changed by the new
             fact being added (affects other Facts' start/end/deleted attrs).
@@ -90,41 +124,164 @@ def add_fact(
     #
     #         tl;dr: factoid is a tuple of 1+ strings that together specify the Fact.
 
-    fact = must_create_fact_from_factoid(
+    # Make a new Fact from the command line input.
+    new_fact = must_create_fact_from_factoid(
         controller, factoid, time_hint, ask,
     )
 
-    if ask:
-        interrogate.ask_user_for_edits(controller, fact)
-
-    new_fact = mend_facts_confirm_and_save_maybe(
-        controller, fact, time_hint, yes, dry,
+    # If there's an ongoing Fact, we might extend or squash it.
+    # Also, if the new Fact overlaps existing Facts, those Facts'
+    # times might be changed, and/or existing Facts might be deleted.
+    new_fact_or_two, conflicts = mend_fact_timey_wimey(
+        controller, new_fact, time_hint,
     )
 
-    return new_fact
+    edit_facts = []
+    orig_facts = []
+    new_fact_pk = -1
+    for new_fact in new_fact_or_two:
+        # If ongoing was squashed, edited_fact.pk > 0, else < 0.
+        # There might also be an extended filler gap Fact added.
+        if new_fact.pk is None:
+            new_fact.pk = new_fact_pk
+            new_fact_pk -= 1
+            edit_facts.append(new_fact)
+        else:
+            # The edited fact is in conflicts, and carousel will start on it.
+            assert new_fact.pk > 0
+
+    for edited, original in conflicts:
+        edit_facts.append(edited)
+        # (lb): This is the only place orig_facts is not [edit_fact.copy(), ...].
+        orig_facts.append(original)
+
+    if use_carousel:
+        saved_facts = prompt_and_save(controller, edit_facts, orig_facts)
+    else:
+        edited_fact = edit_facts[0]
+        interrogate.ask_user_for_edits(controller, edited_fact) if ask else None
+        # Verify at least start time is set; end may or may not be set.
+        time_hint = 'verify_last'
+        saved_facts = mend_facts_confirm_and_save_maybe(
+            controller, edited_fact, time_hint, other_edits={}, yes=yes, dry=dry,
+        )
+
+    return saved_facts
 
 
 # ***
 
-def mend_facts_confirm_and_save_maybe(controller, fact, time_hint, yes, dry):
-    old_end = fact.end
-    if fact.end is None:
-        fact.end = controller.now
-    new_facts = [fact, ]
-    must_complete_times(controller, new_facts, ongoing_okay=True, leave_blanks=True)
-    fact.end = old_end
+def mend_facts_confirm_and_save_maybe(
+    controller, fact, time_hint, other_edits, yes, dry,
+):
+    """"""
+    def _mend_facts_confirm_and_save_maybe():
+        new_fact_or_two, conflicts = mend_fact_timey_wimey(
+            controller, fact, time_hint, other_edits,
+        )
+        saved_facts = confirm_conflicts_and_save_all(new_fact_or_two, conflicts)
+        return saved_facts
 
-    # Fill in the start and, or, end times, maybe.
-    # Possibly correct the times of 2 other Facts!
-    # Or die if too many Facts are abound tonight.
-    conflicts = mend_facts_times(controller, fact, time_hint)
+    def confirm_conflicts_and_save_all(new_fact_or_two, conflicts):
+        """"""
+        # Ask user what to do about conflicts/edits.
+        ignore_pks = other_edits.keys()
+        must_confirm_fact_edits(controller, conflicts, yes, dry)
+        saved_facts = save_facts_maybe(
+            controller, new_fact_or_two, conflicts, ignore_pks, dry,
+        )
+        return saved_facts
 
-    # Ask user what to do about conflicts/edits.
-    must_confirm_fact_edits(controller, conflicts, yes, dry)
+    return _mend_facts_confirm_and_save_maybe()
 
-    new_fact = save_facts_maybe(controller, fact, conflicts, dry)
 
-    return new_fact
+def mend_fact_timey_wimey(controller, fact, time_hint, other_edits={}):
+    """"""
+    def _mend_fact_timey_wimey():
+        must_complete_time(controller, fact, other_edits)
+        # Fill in the start and, or, end times, maybe.
+        # Possibly correct the times of 2 other Facts!
+        # Or die if too many Facts are abound tonight.
+        conflicts = mend_facts_times(controller, fact, time_hint)
+        # Resolve conflicts from store with other edited facts being saved.
+        conflicts = rebuild_conflicts(fact, conflicts, other_edits)
+        new_fact_or_two = unite_and_stretch_fact_per_conflicts(conflicts)
+        return new_fact_or_two, conflicts
+
+    def unite_and_stretch_fact_per_conflicts(conflicts):
+        # On `to` and `then`, combine fact and latest.
+        # Note that insert_forcefully will handle ``to`` for ongoing fact;
+        # otherwise unite_and_stretch handles it for ended latest.
+        if fact.deleted:
+            return []
+        return unite_and_stretch(controller, fact, time_hint, conflicts)
+
+    def must_complete_time(controller, fact, other_edits):
+        reset_end = fact.end is None
+        if fact.end is None:
+            fact.end = controller.now
+        new_facts = [fact, ]
+        must_complete_times(
+            controller,
+            new_facts,
+            ongoing_okay=True,
+            leave_blanks=True,
+            other_edits=other_edits,
+        )
+        assert len(new_facts) == 1
+        fact.end = None if reset_end else fact.end
+
+    def rebuild_conflicts(fact, conflicts, other_edits):
+        if not other_edits:
+            return conflicts
+        culled_conflicts = []
+        for conflict in conflicts:
+            verify_conflict(fact, conflict, other_edits, culled_conflicts)
+        return culled_conflicts
+
+    def verify_conflict(fact, conflict, other_edits, culled_conflicts):
+        auto_edit, orig_fact = conflict
+        assert auto_edit.pk == orig_fact.pk
+        try:
+            edit_fact = other_edits[orig_fact.pk]
+        except KeyError:
+            # Fact was not separately edited, so conflict from store stands.
+            culled_conflicts.append(conflict)
+        else:
+            new_conflict = recheck_conflict(edit_fact, orig_fact)
+            if new_conflict is not None:
+                culled_conflicts.append(new_conflict)
+
+    def recheck_conflict(edit_fact, orig_fact):
+        # Both Facts should have a start, and only 1 may be unended.
+        assert fact.start and edit_fact.start
+        assert fact.end or edit_fact.end
+        if edit_fact.end is None:
+            # If edit_fact is ongoing, should start after fact.
+            if edit_fact.start >= fact.end:
+                return None
+        elif fact.end is None:
+            # If fact is ongoing, should start after edit_fact.
+            if fact.start >= edit_fact.end:
+                return None
+        elif edit_fact.start == edit_fact.end:
+            # Momentaneous can only happen on boundary.
+            if (edit_fact.start <= fact.start) or (edit_fact.start >= fact.end):
+                return None
+        elif fact.start == fact.end:
+            # Momentaneous can only happen on boundary.
+            if (fact.start <= edit_fact.start) or (fact.start >= edit_fact.end):
+                return None
+        elif (edit_fact.end <= fact.start) or (edit_fact.start >= fact.end):
+            # Both facts complete, neither is momentaneous, and ranges are distinct.
+            return None
+        # If we made it here, whoa! Unexpected time conflict after using carousel?
+        new_conflict = (edit_fact, orig_fact, )
+        return new_conflict
+
+    # ***
+
+    return _mend_fact_timey_wimey()
 
 
 # ***
@@ -137,19 +294,20 @@ def must_create_fact_from_factoid(
         controller, factoid, time_hint, ask,
     ):
         separators = must_prepare_factoid_item_separators(controller)
+        use_hint = reduce_time_hint(time_hint)
         try:
-            fact, err = Fact.create_from_factoid(
-                factoid,
-                time_hint=time_hint,
+            fact, err = PlaceableFact.create_from_factoid(
+                factoid=factoid,
+                time_hint=use_hint,
                 separators=separators,
-                lenient=ask,
+                lenient=True,
             )
-            if err:
-                controller.client_logger.info(str(err))
+            controller.client_logger.info(str(err)) if err else None
         except ParserException as err:
             msg = _('Oops! {}').format(err)
             controller.client_logger.error(msg)
             dob_in_user_exit(msg)
+        fact_set_start_time_after_hack(fact, time_hint)
         return fact
 
     def must_prepare_factoid_item_separators(controller):
@@ -165,6 +323,14 @@ def must_create_fact_from_factoid(
             controller.client_logger.error(msg)
             dob_in_user_exit(msg)
         return separators
+
+    # FIXME/DRY: See create.py/transcode.py.
+    def fact_set_start_time_after_hack(fact, time_hint):
+        if time_hint != "verify_after":
+            return
+        assert fact.start is None and fact.end is None
+        # (lb): How's this for a hack!?
+        fact.start = "+0"
 
     # ***
 
@@ -184,16 +350,20 @@ def must_confirm_fact_edits(controller, conflicts, yes, dry):
         if not yes:
             echo_confirmation_banner(conflicts)
         n_conflict = 0
+        n_confirms = 0
         for edited_fact, original in conflicts:
             n_conflict += 1
             if not yes:
-                echo_confirmation_edited(
+                confirmed = echo_confirmation_edited(
                     n_conflict, edited_fact, original,
                 )
+                n_confirms += 1 if confirmed else 0
             else:
                 controller.client_logger.debug(
                     _('Editing fact: {}').format(edited_fact)
                 )
+        if n_conflict != n_confirms:
+            dob_in_user_exit(_("Please try again."))
 
     def cull_stopped_ongoing(conflicts):
         return [con for con in conflicts if 'stopped' not in con[0].dirty_reasons]
@@ -212,17 +382,21 @@ def must_confirm_fact_edits(controller, conflicts, yes, dry):
 
     def echo_confirmation_edited(n_conflict, edited_fact, original):
         click.echo()
-        click.confirm(
+        # MAYBE:
+        #   from .helpers.re_confirm import confirm
+        #   confirmed = confirm()
+        confirmed = click.confirm(
             text=_('Conflict #{}\n-----------\n{}\nReally edit fact?').format(
                 n_conflict,
                 original.friendly_diff(edited_fact),
             ),
             default=False,
-            abort=True,
+            abort=False,
             # prompt_suffix=': ',
             # show_default=True,
             # err=False,
         )
+        return confirmed
 
     # ***
 
@@ -231,22 +405,24 @@ def must_confirm_fact_edits(controller, conflicts, yes, dry):
 
 # ***
 
-def save_facts_maybe(controller, fact, conflicts, dry):
+def save_facts_maybe(controller, new_facts, conflicts, ignore_pks, dry):
     """"""
 
-    def _save_facts_maybe(controller, fact, conflicts, dry):
+    def _save_facts_maybe(controller, new_facts, conflicts, ignore_pks, dry):
+        new_and_edited = []
         if conflicts and dry:
             echo_dry_run()
         for edited_fact, original in conflicts:
             if not dry:
-                save_fact(controller, edited_fact, dry)
+                new_and_edited += save_fact(controller, edited_fact, dry)
                 if 'stopped' in edited_fact.dirty_reasons:
                     echo_ongoing_completed(controller, edited_fact)
             else:
                 click.echo(original.friendly_diff(edited_fact))
 
-        new_fact = save_fact(controller, fact, dry)
-        return new_fact
+        for fact in new_facts:
+            new_and_edited += save_fact(controller, fact, dry, ignore_pks=ignore_pks)
+        return new_and_edited
 
     def echo_dry_run():
         click.echo()
@@ -255,34 +431,27 @@ def save_facts_maybe(controller, fact, conflicts, dry):
             attr('reset'),
         ))
 
-    def save_fact(controller, fact, dry):
+    def save_fact(controller, fact, dry, ignore_pks=[]):
+        if fact.pk and fact.pk < 0:
+            fact.pk = None
+        if fact.pk is None and fact.deleted:
+            controller.client_logger.debug('{}: {}'.format(_('Dead fact'), fact))
+            return []
         if not dry:
             controller.client_logger.debug('{}: {}'.format(_('Save fact'), fact))
             try:
-                new_fact = controller.facts.save(fact)
+                new_fact = controller.facts.save(fact, ignore_pks=ignore_pks)
             except Exception as err:
+                traceback.print_exc()
                 dob_in_user_exit(str(err))
         else:
             new_fact = fact
             echo_fact(fact)
-        return new_fact
+        return [new_fact, ]
 
     # ***
 
-    return _save_facts_maybe(controller, fact, conflicts, dry)
-
-
-def echo_fact(fact):
-    click.echo('{}Dry run! New fact{}:\n '.format(
-        attr('underlined'),
-        attr('reset'),
-    ))
-    click.echo('{}{}{}{}'.format(
-        fg('steel_blue_1b'),
-        attr('bold'),
-        fact.friendly_str(description_sep='\n\n'),
-        attr('reset'),
-    ))
+    return _save_facts_maybe(controller, new_facts, conflicts, ignore_pks, dry)
 
 
 # ***
@@ -354,16 +523,10 @@ def echo_ongoing_completed(controller, fact, cancelled=False):
             fact.friendly_str(
                 shellify=False,
                 description_sep=': ',
-
-                # FIXME/2018-06-10: (lb): fact being saved as UTC
+                # FIXME: (lb): Implement localize.
                 localize=True,
-
                 colorful=colorful,
-
-                # FIXME/2018-06-12: (lb): Too wide (wraps to next line);
-                # doesn't account for leading fact parts (times, act@gory, tags).
                 cut_width=cut_width,
-
                 show_elapsed=True,
             )
         )
