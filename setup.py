a7d12e01ea945931cbd89e6674a84557e847c61b
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-02-19 22:03:29 -0600

    Docs: Add copyright header to setup.py.
    - Also add a few links to module docstring.

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This file is part of 'dob'.
+#
+# 'dob' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'dob' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'dob'.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Packaging instruction for setup tools.
 
+Refs:
+
   https://setuptools.readthedocs.io/
+
+  https://packaging.python.org/en/latest/distributing.html
+
+  https://github.com/pypa/sampleproject
 """
 
 import os
