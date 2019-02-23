c62c6af33015aaee78ee67bc3c3e39a90d969cf7
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-02-22 19:14:54 -0600

    Docs: Add suppress_warnings, to silence non-local image warnings.

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,9 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+# Prevent non local image warnings from showing.
+suppress_warnings = ['image.nonlocal_uri']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
