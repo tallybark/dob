3b1725c76f6c10b3577ad79d8f3628deb119d4c5
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-11-15 15:32:21 -0600

    DX: Comment: Different ways to handle -h/--help, and dis/advantages to each.

--- dob/cmd_interface/cmd_common.py
+++ dob/cmd_interface/cmd_common.py
@@ -288,6 +288,22 @@ def show_help_finally(func):
     instead, i.e., `dob --help command` will now show the help for
     command."""
 
+    # (lb): I'll admit this might be the "hard way" to do this.
+    # In run_cli.py, rather than passing help_option_names to
+    # the Context, we could add our own click.option for -h/--help,
+    # add a 'help' param to the run() method, and set the flag that
+    # way. However, then each command would need to set the help
+    # option, e.g., `dob --help command` sets help on the root Context
+    # (via the run_cli.run() callback), whereas `dob command --help`
+    # sets the help option on the command's context. Not that this
+    # would be hard to do -- in addition to adding the show_help_finally
+    # decorator to every Click command in dob.py, we could also add a
+    # new click.option decorator to each of those commands... but then
+    # we'd also have to add the 'help' param to every command method in
+    # dob.py... and you see right there what a colossal pain using a
+    # normal Click option to handle --help would be. So I guess it is
+    # not the "hard way" having Click specially handle -h/--help.
+
     @click.pass_context
     def check_help(ctx, *args, **kwargs):
         if ctx.find_root().help_option_spotted:
