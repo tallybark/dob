c79d81e50df551fd1176de7115d0deb021b7e96e
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-11-18 20:16:39 -0600

    Linting: Add missing base package module (__init__.py).

---
+++ dob/clickux/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of 'dob'.
+#
+# 'dob' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'dob' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'dob'.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Top-level package module for clickux modules."""
+
+from __future__ import absolute_import, unicode_literals
+
