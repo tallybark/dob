ab5d97797b511f74307de043e75536bdba06ac91
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-12-06 00:11:09 -0600

    Bugfix: Cull Facts with no PK before making pk range text.
    - I think it was a log.debug call (I've since discared the stack trace)
      that stringified GroupChained, and a Fact with pk == None broke the
      __str__ method.
      - Also, DX: I stuck in a dev-stop (set_trace) to monitor this issue.

--- dob/helpers/__init__.py
+++ dob/helpers/__init__.py
@@ -131,21 +131,7 @@ def integer_range_groupify(integers):
     from operator import itemgetter
 
     def _integer_range_groupify():
-        try:
-            srtd = sorted(map(int, integers))
-        except Exception as err:
-            # 2019-12-03 01:16: On add tag, then save. Died was jump_fact_inc
-            # or @decorator? (not sure what I was doing; and there were two
-            # stack traces:)
-            #  ...
-            #  File "dob/dob/traverser/key_action_map.py", line 88, in jump_fact_inc
-            #    self.zone_manager.jump_fact_inc(event)
-            #  ...
-            #  Traceback (most recent call last):
-            #  ...
-            #  ValueError: invalid literal for int() with base 10: 'None'
-            self.carousel.controller.affirm(False)
-
+        srtd = sorted(map(int, integers))
         return assemble_groups(srtd)
 
     def range_key_recursive(srtd, ix):
