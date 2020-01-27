6a321ae591209c52fd086d2839d22b7cb732ecf3
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-01-26 23:47:15 -0600

    Docs: Update copyright headers.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,19 @@
-# This file is part of 'dob'.
+# This file exists within 'dob':
 #
-# 'dob' is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   https://github.com/hotoffthehamster/dob
 #
-# 'dob' is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Copyright © 2018-2020 Landon Bouma,  2015-2016 Eric Goller.  All rights reserved.
 #
-# You should have received a copy of the GNU General Public License
-# along with 'dob'.  If not, see <http://www.gnu.org/licenses/>.
+# 'dob' is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License  as  published by the Free Software Foundation,
+# either version 3  of the License,  or  (at your option)  any   later    version.
+#
+# 'dob' is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY  or  FITNESS FOR A PARTICULAR
+# PURPOSE.  See  the  GNU General Public License  for  more details.
+#
+# You can find the GNU General Public License reprinted in the file titled 'LICENSE',
+# or visit <http://www.gnu.org/licenses/>.
 
 """Testsuite for ``dob``."""
 
